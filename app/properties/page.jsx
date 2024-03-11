ee0d5247767c510cbe2e57a6accbe6d585d7c9d2
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2024-03-12 01:25:44 +0200

    fetch data with server component

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,15 +1,35 @@
 import PropertyCard from "@/components/PropertyCard";
-import properties from "@/properties.json";
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+
+  // sort by date
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+  console.log(properties);
+
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto">
-        {properties.length === 0 ? (
+        {properties?.length === 0 ? (
           <p>No properties found</p>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
+            {properties?.map((property) => (
               <PropertyCard key={property._id} property={property} />
             ))}
           </div>
