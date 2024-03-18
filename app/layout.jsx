c9614df7043242600279f6d4b696025f14c4d7ec
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2024-03-18 19:40:24 +0200

    toast notifications

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,9 @@ import "@/assets/styles/globals.css";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
   description: "Find your dream rental property",
@@ -17,6 +20,7 @@ const MainLayout = ({ children }) => {
           <Navbar />
           <main>{children}</main>
           <Footer />
+          <ToastContainer />
         </body>
       </html>
     </AuthProvider>
